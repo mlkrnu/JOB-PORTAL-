64a220b5252f5624e213b1b37c6eb43759a0095e
mlkrnu <tanishqkumar3435g@gmail.com>
2026-07-02 23:45:12 +0530

    Fixed job description

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Badge } from './ui/badge';
 import { Button } from './ui/button';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams,useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { APPLICATION_API_END_POINT, JOB_API_END_POINT } from '@/utils/constant';
 import { setSingleJob } from '@/redux/jobSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'sonner';
-import { useNavigate, useParams, useLocation } from "react-router-dom";
 
 const JobDescription = () => {
     const { singleJob } = useSelector(store => store.job);
