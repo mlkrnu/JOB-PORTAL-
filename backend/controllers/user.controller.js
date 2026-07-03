26b05bfdd4e55360cb21cda924f21968f868a01f
mlkrnu <tanishqkumar3435g@gmail.com>
2026-07-03 15:59:19 +0530

    fixed usercontroller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -96,7 +96,7 @@ export const login = async (req, res) => {
             maxAge: 1 * 24 * 60 * 60 * 1000,
             httpOnly: true,
             secure: true,
-            sameSite: "none",
+            sameSite: "lax",
         }).json({
             message: `Welcome back ${user.fullname}`,
             user,
